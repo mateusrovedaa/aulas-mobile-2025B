5143030cea86d6474d94ecb80c023b9a8f18ec66
MATEUS ROVEDA <mateus.roveda@acad.univates>
2025-08-06 21:47:37 -0300

    Adiciona botão e flatlist

--- aula0608-componentes/App.js
+++ aula0608-componentes/App.js
@@ -1,4 +1,25 @@
-import { View, Text } from "react-native";
+import { View, Text, Button, FlatList } from "react-native";
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+
+const Item = ({ title }) => (
+  <View>
+    <Text>{title}</Text>
+  </View>
+);
 
 export default function App() {
   return (
@@ -10,6 +31,16 @@ export default function App() {
       }}
     >
       <Text>Pipoca</Text>
+      <Button
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+      <FlatList
+        data={DATA}
+        renderItem={({ item }) => <Item title={item.title} />}
+        keyExtractor={item => item.id}
+      />
     </View>
   );
 }
